e6a2cc0edb79da1e19cbe9dacd752d1510852f49
Akhil Vengassery <akhillav@gmail.com>
2023-10-18 12:10:43 +0530

    home page link

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const ham = document.querySelector(".ham");
 const list = document.querySelector("ul");
 
+const btn = document.querySelector(".btn");
+
 ham.addEventListener("click", (e) => {
   list.classList.toggle("show");
 });
@@ -10,12 +12,15 @@ const text = document.querySelector(".sec-text");
 
 const textLoad = () => {
   setTimeout(() => {
+    btn.href = "./gallery.html";
     text.textContent = "GALLERY";
   }, 0);
   setTimeout(() => {
+    btn.href = "./articles.html";
     text.textContent = "ARTICLES";
   }, 4000);
   setTimeout(() => {
+    btn.href = "./photos.html";
     text.textContent = "PHOTOS";
   }, 8000);
 };
